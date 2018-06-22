f1a9b2d762c7a7798c950779e4973b639f1b0562
innsd <innsd@qq.com>
2018-06-22 15:46:35 +0800

    begin

---
+++ permission_merge_tool.py
@@ -0,0 +1,7 @@
+######################################################################################
+#The tool which is used to merge permission of minecraft game plugin GroupManner     #
+#author:insnd                                                                        #
+#e-mail:innsd@qq.com                                                                 #
+######################################################################################
+import yaml
+
