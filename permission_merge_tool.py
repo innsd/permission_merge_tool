08943c61827ff1173561d1523ffe5dba28a9c514
innsd <innsd@qq.com>
2018-06-26 15:59:43 +0800

    change data structure

--- permission_merge_tool.py
+++ permission_merge_tool.py
@@ -18,13 +18,12 @@ logger.info('----------------------程序启动----------------------')
 try:
     with open("config.json","r") as f:
         config_data=json.load(f)
-    logging.info("1服路径："+config_data['path1'])
-    logging.info("2服路径："+config_data['path2'])
-    logging.info("3服路径："+config_data['path3'])
+    for servername,path in config_data['paths'].items():
+        logging.info("文件路径：")
+        logging.info(servername+":"+path)
     logging.info("间隔时间："+str(config_data['interval']))
     logging.info("是否备份："+str(config_data['backups']))
     logger.info("*****************************************************")
-#        print(config_data)
 except FileNotFoundError:
     print("第一次使用本程序？请完成以下初始化设置：")
     file_init()
